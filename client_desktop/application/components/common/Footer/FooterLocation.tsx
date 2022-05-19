04329700eb8e940398d4d6e6922c6272d74ef7db
git4res <akuzin20@gmail.com>
2022-05-19 18:37:28 +0300

    style fixes

--- client_desktop/application/components/common/Footer/FooterLocation.tsx
+++ client_desktop/application/components/common/Footer/FooterLocation.tsx
@@ -5,16 +5,19 @@ const FooterLocation = () => {
     const useCaseLocationHeader = adapterComponentUseCase(useHeaderLocations);
     const { selectedPoint } = useCaseLocationHeader.data;
     const {handlerHeader } = useCaseLocationHeader.handlers;
+    console.log('selectedPoint', selectedPoint);
     return (
         <div className="header_adress-info" onClick={handlerHeader}>
             <div className="footer_box-title">{selectedPoint.city}</div>
             <div className="footer_box_address_line">{selectedPoint.address}</div>
             <div className="footer_box_address_line phones">{selectedPoint.phone}</div>
-            <div>
-                <a href="https://t.me/starikhinkalych" target="_blank" rel="noreferrer">
-                    <img className="tg" src="/images/icon/tg-plane.png" alt="" />
-                </a>
-            </div>
+            {Object.keys(selectedPoint).length !== 0 &&
+                <div>
+                    <a href="https://t.me/starikhinkalych" target="_blank" rel="noreferrer">
+                        <img className="tg" src="/images/icon/tg-plane.png" alt="" />
+                    </a>
+                </div>
+            }
         </div>
     );
 };
