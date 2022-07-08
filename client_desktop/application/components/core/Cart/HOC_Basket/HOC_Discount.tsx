a5066884cb36258833a3c140208aa8f9ecd9efe7
xirurgx3d <xirurgx3d@gmail.com>
2022-07-08 11:21:24 +0300

    Create HOC_Discount.tsx

---
+++ client_desktop/application/components/core/Cart/HOC_Basket/HOC_Discount.tsx
@@ -0,0 +1,12 @@
+import SpecialOfferFree from "application/components/common/SpecialOffer/SpecialOfferGift"
+import SpecialOfferLeft from "application/components/common/SpecialOffer/SpecialOfferLeft"
+
+const Discounts = () =>{
+	return(
+		<>
+		<SpecialOfferFree/>
+		<SpecialOfferLeft/>
+		</>
+	)
+}
+export default Discounts
