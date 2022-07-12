7b92d68bb4a758cbd47f0aa2c1125a4e79f1b67f
git4res <akuzin20@gmail.com>
2022-07-12 17:34:42 +0300

    Point closed comment for mobile

--- client/src/application/components/core/Cart/CartBasket/CartTotal.tsx
+++ client/src/application/components/core/Cart/CartBasket/CartTotal.tsx
@@ -3,7 +3,7 @@ import { adapterSelector } from 'servises/redux/selectors/selectors';
 const CartTotal = () => {
   const {totalPrice} = adapterSelector.useSelectors(selector => selector.cart)
   return (
-    <div className="cart__order-info"> 
+    <div className="cart__order-info">
             {
                 /*
                 <div className="cart__order__sale-wrap">
@@ -13,13 +13,24 @@ const CartTotal = () => {
 
                     <div className="cart__order__sale">0 ₽</div>
                 </div>
-                
+
                 */
             }
             <div className="cart__order__total-wrap">
                 <div className="cart__order__total-title">Итого</div>
                 <div className="cart__order__total-sum">{totalPrice} ₽</div>
             </div>
+        {/* point closed cart*/}
+        {false &&
+            <div className="point-closed-cart-container">
+                <div className="top-text">Хинкальная сейчас закрыта.<br/>
+                    Оформить заказ вы сможете: <span>9:00-21:30</span></div>
+                <div className="text-secondary">
+                    Приносим извинения за неудобства. <br/>
+                    Сейчас вы можете ознакомиться с меню для будущих заказов и узнать об акциях и новинках.
+                </div>
+            </div>
+        }
         </div>
   )
 }
