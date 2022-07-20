3779b0abb7fdce938f41d75779964435364d53f1
git4res <akuzin20@gmail.com>
2022-07-21 01:11:50 +0300

    pickup styles fixed

--- client/src/presentation/layout/Cart/CartLayout.tsx
+++ client/src/presentation/layout/Cart/CartLayout.tsx
@@ -31,29 +31,27 @@ const CartLayout: FC<ICartLayout> = ({ children }) => {
                   <Discounts />
                   <CartDeliveryPrice />
                   <CartTotal />
-                  {/*{workTimeHelp()*/}
-                  {/*    ? <>*/}
-                  {/*        <div className="point-closed-cart-container">*/}
-                  {/*            <div className="top-text">Хинкальная сейчас закрыта.<br />*/}
-                  {/*                Оформить заказ вы сможете: <span>{point.workTime}</span></div>*/}
-                  {/*            <div className="text-secondary">*/}
-                  {/*                Приносим извинения за неудобства. <br />*/}
-                  {/*                Сейчас вы можете ознакомиться с меню для будущих заказов и узнать об акциях и*/}
-                  {/*                новинках.*/}
-                  {/*            </div>*/}
-                  {/*        </div>*/}
-                  {/*        <button*/}
-                  {/*            type="submit"*/}
-                  {/*            className="order-btn-pointclosed"*/}
-                  {/*            disabled={true}*/}
-                  {/*        >*/}
-                  {/*            Заказать*/}
-                  {/*        </button>*/}
-                  {/*    </>*/}
-                  {/*    : children*/}
-                  {/*}*/}
-                  {children}
-
+                  {workTimeHelp()
+                      ? <>
+                          <div className="point-closed-cart-container">
+                              <div className="top-text">Хинкальная сейчас закрыта.<br />
+                                  Оформить заказ вы сможете: <span>{point.workTime}</span></div>
+                              <div className="text-secondary">
+                                  Приносим извинения за неудобства. <br />
+                                  Сейчас вы можете ознакомиться с меню для будущих заказов и узнать об акциях и
+                                  новинках.
+                              </div>
+                          </div>
+                          <button
+                              type="submit"
+                              className="order-btn-pointclosed"
+                              disabled={true}
+                          >
+                              Заказать
+                          </button>
+                      </>
+                      : children
+                  }
               </div>
           </div>
       </div>
