1e825cfd330c1bbc319b18d160ac742c92e18634
git4res <akuzin20@gmail.com>
2022-08-02 15:14:31 +0300

    logo choose city

--- client/src/presentation/layout/Location/CitiListLayout.tsx
+++ client/src/presentation/layout/Location/CitiListLayout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { FC, ReactNode } from "react";
 
 const CitiListLayout:FC<{children:ReactNode}> = ({children}) => (
@@ -5,7 +6,11 @@ const CitiListLayout:FC<{children:ReactNode}> = ({children}) => (
         <div className="welcome__header">
             <div className="container row justify-between align-center">
                 <div className="welcome__header__content">
-                    Выберите <span className="select-red">город</span>
+                    <img
+                        src={require("assets/img/logo-choose-city.svg").default}
+                        alt="Доставка и самовывоз"
+                    />
+                    <span className="choose">Выберите <span className="select-red">город</span></span>
                 </div>
             </div>
         </div>
