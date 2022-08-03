0b97b27e452b080459418269a520fd832ccd6fb0
git4res <akuzin20@gmail.com>
2022-08-03 11:40:25 +0300

    delivery cost fix

--- client/src/application/components/core/Cart/CartBasket/DeliveryCost.tsx
+++ client/src/application/components/core/Cart/CartBasket/DeliveryCost.tsx
@@ -9,10 +9,8 @@ const DeliveryCost = () => {
         <div className="cart__memo">
             {
                 orderType === CART_CHOICE.COURIER && (
-                    deltaPrice === 0
-                        ? <div className="cart__memo__banner free-deliv"><span className="ok-icon"></span>Бесплатная
-                            доставка от 700 ₽</div>
-                        : <div className="cart__memo__banner ">
+                    deltaPrice !== 0
+                        && <div className="cart__memo__banner ">
                             <img src={require("assets/i/moto.svg").default} alt="стрелка" />
                             <div>До бесплатной доставки <br/> закажите на сумму: <b>{deltaPrice} ₽</b></div>
                         </div>
