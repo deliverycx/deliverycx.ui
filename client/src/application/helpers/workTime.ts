5383a61fc3cd66070ce16ff0df9d95b7fea86ca6
xirurgx3d <xirurgx3d@gmail.com>
2022-11-09 18:44:03 +0300

    Update workTime.ts

--- client/src/application/helpers/workTime.ts
+++ client/src/application/helpers/workTime.ts
@@ -8,6 +8,7 @@ export const workTimeHelp = (work?:string) =>{
 	if(workTime || work){
 		const mok2 = "10:00-12:00"
 		const mok = workTime || work
+		console.log('mokkkkk',mok,workTime);
 		const [min,max] = mok ? mok.split('-') : workTime.split('-')
 		const time = format(new Date(), "HH:mm")
 
