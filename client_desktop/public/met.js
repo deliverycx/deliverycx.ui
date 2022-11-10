6263d88f3a40f1b7d6c68727491e4caaa2eefafd
xirurgx3d <xirurgx3d@gmail.com>
2022-11-10 17:08:15 +0300

    metrik

---
+++ client_desktop/public/met.js
@@ -0,0 +1,1 @@
+!function(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.src='https://vk.com/js/api/openapi.js?169',t.onload=function(){VK.Retargeting.Init("VK-RTRG-1591066-9fa2W"),VK.Retargeting.Hit()},document.head.appendChild(t)}();
