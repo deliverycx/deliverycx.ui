49bc04d9ed343ee0864be1e5434efdceb6bc2517
xirurgx3d <xirurgx3d@gmail.com>
2022-11-10 17:53:35 +0300

    ксс заказ не доступен

--- client_desktop/application/components/core/Location/HOC_Points/view/PointDisplay.tsx
+++ client_desktop/application/components/core/Location/HOC_Points/view/PointDisplay.tsx
@@ -10,7 +10,7 @@ const PointDisplay:FC<{setter:any}> = ({setter}) =>{
 									Оформление онлайн заказа в данной <br /> хинкальной недоступно.
 							</div>
 							<div className="secondary-text">
-									А пока вы можете ознакомится с нашим меню<br/> и почитать новости
+									Вы можете ознакомится с нашим меню<br/> и почитать новости
 							</div>
 							<div className="notification_worktime-buttons">
 									{/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
