0bcd64fec3af6dce92095316a1ef9242d8bc5671
xirurgx3d <xirurgx3d@gmail.com>
2022-12-08 18:43:55 +0300

    урл дестоп

--- client_desktop/domain/use-case/useCaseLocation/useCase.Points.ts
+++ client_desktop/domain/use-case/useCaseLocation/useCase.Points.ts
@@ -38,7 +38,7 @@ export function usePoints(this: any,{selectCity,handleSelectOrganitztion}:any) {
     }
     
     RequestProfile.update({ organizationId: address.id });
-    router.push(ROUTE_APP.MENU)
+    router.push(`${ROUTE_APP.MENU}/?organization=${address.guid}`)
   }
 
 	console.log(cityid);
