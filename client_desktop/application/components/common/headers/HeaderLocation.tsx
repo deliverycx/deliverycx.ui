b8df8c697df473ceb5a9a58170d66328644bd839
xirurgx3d <xirurgx3d@gmail.com>
2022-12-14 14:14:08 +0300

    фикс десктоп

--- client_desktop/application/components/common/headers/HeaderLocation.tsx
+++ client_desktop/application/components/common/headers/HeaderLocation.tsx
@@ -7,20 +7,30 @@ const HeaderLocation = () => {
     const {handlerHeader } = useCaseLocationHeader.handlers;
 
 		const rend = () =>{
-			window.location.href = process.env.NEXT_PUBLIC_REDIRECT as string
+			//window.location.href = process.env.NEXT_PUBLIC_REDIRECT as string
 		}
 
     return (
         <div className="header_adress-info">
 						{
 							/**
-							 * 
-							 * <span onClick={() => handlerHeader('city')} className="header_adress-info-active">{selectedPoint.city}</span>
+						 
+						 <span onClick={() => handlerHeader('city')} className="header_adress-info-active">{selectedPoint.city}</span>
             <span onClick={() => handlerHeader('point')}>{selectedPoint.address}</span>
 							 */
 						}
-            <span onClick={() => rend()} className="header_adress-info-active">{selectedPoint.city}</span>
+						<span onClick={() => handlerHeader('city')} className="header_adress-info-active">{selectedPoint.city}</span>
+            <span onClick={() => handlerHeader('point')}>{selectedPoint.address}</span>
+						{
+							/**
+							 * <span onClick={() => rend()} className="header_adress-info-active">{selectedPoint.city}</span>
             <span onClick={() => rend()}>{selectedPoint.address}</span>
+							 * 
+							 */
+						}
+            
+
+
             <a href={`tel:${selectedPoint.phone}`} className="phones">{selectedPoint.phone}</a>
         </div>
     );
