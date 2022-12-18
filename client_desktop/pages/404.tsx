0b8b71eec6e1699b8c1319ff8d4a1fa253ea5f36
xirurgx3d <xirurgx3d@gmail.com>
2022-12-18 16:53:38 +0300

    404

--- client_desktop/pages/404.tsx
+++ client_desktop/pages/404.tsx
@@ -1,19 +1,13 @@
+import ErrorPage from "application/components/common/ErrorPages/NotFoundError/ErrorPage";
 import Footer from "application/components/common/Footer/Footer";
 import Header from "application/components/common/headers/Header";
 import React from "react";
 
-const ErrorPage = () => {
+const ErrorPage404 = () => {
   return (
-		<>
-    <div className="container">
-			<Header />
-			<h1>404 =)</h1>
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&amp;start=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-    </div>
-		<br />
-		<Footer />
-		</>
+		<ErrorPage />
   );
 };
 
-export default ErrorPage;
+export default ErrorPage404;
+
