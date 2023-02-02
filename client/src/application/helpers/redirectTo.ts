81154c1f3d4f6d73b9438e596e388173c0c7560f
xirurgx3d <xirurgx3d@gmail.com>
2023-02-02 16:02:03 +0300

    редиректы с турецкой

--- client/src/application/helpers/redirectTo.ts
+++ client/src/application/helpers/redirectTo.ts
@@ -1,5 +1,5 @@
 export const Redirects = (id:string) =>{
 	if(id === 'fe470000-906b-0025-00f6-08d8de6557e1'){
-		//window.location.href = 'https://%D0%BC%D0%BE%D0%B1.%D1%82%D1%80%D0%B0%D0%BD%D1%81%D1%84%D0%B5%D1%80.%D1%85%D0%B8%D0%BD%D0%BA%D0%B0%D0%BB%D1%8B%D1%87.%D1%80%D1%84/shop?organuzation=7dcec94b-1109-439b-a27a-47ef897289ad'
+		window.location.href = 'https://%D0%BC%D0%BE%D0%B1.%D1%82%D1%80%D0%B0%D0%BD%D1%81%D1%84%D0%B5%D1%80.%D1%85%D0%B8%D0%BD%D0%BA%D0%B0%D0%BB%D1%8B%D1%87.%D1%80%D1%84/shop?organuzation=7dcec94b-1109-439b-a27a-47ef897289ad'
 	}
 }
