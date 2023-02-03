4826f2615d2995db514b0d664913d77deffed92e
xirurgx3d <xirurgx3d@gmail.com>
2023-02-03 11:50:57 +0300

    редирект 2

--- client_desktop/application/helpers/redirectTo.ts
+++ client_desktop/application/helpers/redirectTo.ts
@@ -1,5 +1,5 @@
 export const Redirects = (id:string) =>{
 	if(id === 'fe470000-906b-0025-00f6-08d8de6557e1'){
-		//window.location.href = 'https://трансфер.хинкалыч.рф/?organuzation=7dcec94b-1109-439b-a27a-47ef897289ad'
+		window.location.href = 'https://трансфер.хинкалыч.рф/?organuzation=7dcec94b-1109-439b-a27a-47ef897289ad'
 	}
 }
