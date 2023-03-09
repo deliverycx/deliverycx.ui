e80b7b2ad8086c5c0098c2b0f39dee61b8a3c6b5
xirurgx3d <xirurgx3d@gmail.com>
2023-03-09 14:16:40 +0300

    Update redirectTo.ts

--- client_desktop/application/helpers/redirectTo.ts
+++ client_desktop/application/helpers/redirectTo.ts
@@ -4,6 +4,6 @@ import { RequestAdmin } from "servises/repository/Axios/RequestAdmin"
 export const Redirects = async (id:string) =>{
 	const {data} = await RequestAdmin.getBu({idorganization:id})
 	if(data.redirectON){
-		window.location.href = `https://трансфер.хинкалыч.рф/?organuzation={data.redirect}`
+		window.location.href = `https://трансфер.хинкалыч.рф/?organuzation=${data.redirect}`
 	}
 }
