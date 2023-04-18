e94b038d9108f09ecffe2ca9cae6e4a91092a103
xirurgx3d <xirurgx3d@gmail.com>
2023-04-18 17:27:42 +0300

    q

--- client_desktop/pages/_document.tsx
+++ client_desktop/pages/_document.tsx
@@ -15,6 +15,7 @@ export default class MyDocument extends Document {
 		        />
             <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon"/>
 						<meta name="yandex-verification" content="46bb2570dfd8760a" />
+						<span className="metrika">424242</span>
         </Head>
         <script async src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=e45f9cf9-d514-40a5-adb9-02524aaef83f"></script>
         <body>
