86fd0d73973fbcd3798f98fa09ce010b7a86eaa1
xirurgx3d <xirurgx3d@gmail.com>
2023-04-19 14:09:01 +0300

    Update YMapPoint.tsx

--- client/src/application/components/core/Location/Points/YMapPoint.tsx
+++ client/src/application/components/core/Location/Points/YMapPoint.tsx
@@ -94,11 +94,11 @@ const YMapPoint = () => {
                             width="100"
                             height="100vh"
                             defaultState={{
-                                center: addresses[0] ? [addresses[statePoint.slideIndex].cords[0], addresses[statePoint.slideIndex].cords[1]] : [0.0, 0.0],
+                                center: addresses[0] ? [addresses[statePoint.slideIndex].cords[1], addresses[statePoint.slideIndex].cords[0]] : [0.0, 0.0],
                                 zoom: 13
                             }}
                             state={{
-                                center: addresses[0] ? [addresses[statePoint.slideIndex].cords[0], addresses[statePoint.slideIndex].cords[1]] : [0.0, 0.0],
+                                center: addresses[0] ? [addresses[statePoint.slideIndex].cords[1], addresses[statePoint.slideIndex].cords[0]] : [0.0, 0.0],
                                 zoom: 13,
 
                             }}
