3485ba536d83dd91d1e751cf9e78753b7a94c01d
xirurgx3d <xirurgx3d@gmail.com>
2023-04-20 15:31:14 +0300

    редирект в на олд апи

--- client_desktop/application/helpers/redirectTo.ts
+++ client_desktop/application/helpers/redirectTo.ts
@@ -1,5 +1,10 @@
-export const Redirects = (id:string) =>{
-	if(id === 'fe470000-906b-0025-00f6-08d8de6557e1'){
-		//window.location.href = 'https://%D0%BC%D0%BE%D0%B1.%D1%82%D1%80%D0%B0%D0%BD%D1%81%D1%84%D0%B5%D1%80.%D1%85%D0%B8%D0%BD%D0%BA%D0%B0%D0%BB%D1%8B%D1%87.%D1%80%D1%84/shop?organuzation=7dcec94b-1109-439b-a27a-47ef897289ad'
+
+import { store } from "servises/redux/createStore";
+import { RequestAdmin } from "servises/repository/Axios/RequestAdmin";
+
+export const Redirects = async (id:string) =>{
+	const {data} = await RequestAdmin.getBu({idorganization:id})
+	if(data.redirectON){
+		window.location.href = `https://десктоп.хинкалыч.рф/shop?organuzation=${data.redirect}`
 	}
 }
