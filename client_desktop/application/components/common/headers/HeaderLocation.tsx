ea4ae87bfa33b581aa5a7fb228af24403ed213c6
xirurgx3d <xirurgx3d@gmail.com>
2023-04-20 17:33:31 +0300

    Update HeaderLocation.tsx

--- client_desktop/application/components/common/headers/HeaderLocation.tsx
+++ client_desktop/application/components/common/headers/HeaderLocation.tsx
@@ -7,9 +7,7 @@ const HeaderLocation = () => {
     const {handlerHeader } = useCaseLocationHeader.handlers;
 
 		const h = (q:any) => {
-			if(process.env.NODE_ENV === 'production'){
-				window.location.href = 'https://%D0%B4%D0%B5%D1%81%D0%BA%D1%82%D0%BE%D0%BF.%D1%85%D0%B8%D0%BD%D0%BA%D0%B0%D0%BB%D1%8B%D1%87.%D1%80%D1%84'
-			}
+			window.location.href = 'https://десктоп.хинкалыч.рф'
 		
 		}
 
