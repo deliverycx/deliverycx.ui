406e204463d9edd3818ed51f8f4d35e4af9dc506
xirurgx3d <xirurgx3d@gmail.com>
2023-04-25 19:35:45 +0300

    метрика

--- client_desktop/pages/_document.tsx
+++ client_desktop/pages/_document.tsx
@@ -5,6 +5,17 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+						<title>
+							«Старик Хинкалыч» - крупнейшая сеть хинкальных в России
+		        </title>
+						<meta
+		          name="description"
+		          content="Выбирайте ближайший адрес и попробуйте хинкали от 35 рублей, румяные хачапури и другие блюда грузинской кухни!"
+		          key="desc"
+		        />
+            <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon"/>
+						<meta name="yandex-verification" content="46bb2570dfd8760a" />
+						<span className="metrika">424242</span>
             <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon"/>
         </Head>
         <script async src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=e45f9cf9-d514-40a5-adb9-02524aaef83f"></script>
