939ca31d0d771f2073b93939418fd3b9f236f3ac
xirurgx3d <xirurgx3d@gmail.com>
2023-07-11 12:40:44 +0300

    Update redirectTo.ts

--- client/src/application/helpers/redirectTo.ts
+++ client/src/application/helpers/redirectTo.ts
@@ -7,5 +7,6 @@ export const Redirects = async (adress:IPoint) =>{
 	//const {data} = await RequestAdmin.getBu({idorganization:id})
 	if(adress.redirectON){
 		window.location.href = `https://м.доставка.хинкалыч.рф/shop?organuzation=${adress.redirect}`
+		return
 	}
 }
