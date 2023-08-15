cf6d710ee6ac933145838f15f0b6d131f0498f5a
xirurgx3d <xirurgx3d@gmail.com>
2023-08-15 13:49:04 +0300

    метрика фикс

--- client_desktop/pages/_document.tsx
+++ client_desktop/pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
@@ -20,6 +21,10 @@ export default class MyDocument extends Document {
             <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon"/>
         </Head>
         <script async src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=473431c9-b8f6-45d6-a166-243a0152c68b"></script>
+				<script src='/met.js'/>
+							<noscript><img src="https://vk.com/rtrg?p=VK-RTRG-1591066-9fa2W" className="metrika" alt=""/></noscript>
+							<script src='/yan.js'/>
+							<noscript><div><img src="https://mc.yandex.ru/watch/91135511" className="metrika" /></div></noscript>
         <body>
           <Main />
           <NextScript />
