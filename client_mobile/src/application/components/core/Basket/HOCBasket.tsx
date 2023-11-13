6a4a60b3ee7df455b1113ca606b733a0a9ae794e
GulyaevGSM <sarvargulyaev@yandex.ru>
2023-11-13 14:35:14 +0300

    Фикс модалки очистки корзины

--- client_mobile/src/application/components/core/Basket/HOCBasket.tsx
+++ client_mobile/src/application/components/core/Basket/HOCBasket.tsx
@@ -64,22 +64,22 @@ const HOCBasket = () => {
                 <ModalCard setIsOpened={setIsModalDeleteOpened}>
                     <div className="modal">
                         <div className="modal__wrapper">
-                            <div className="modal__header justify-between">
+                            <div className="modal__header modal__header__fix justify-between">
                                 <div className="d-flex flex-center justify-between">
                                     <div onClick={() => setIsModalDeleteOpened(false)} className="d-flex flex-center justify-center modal__btn no-drag">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                                             <path d="M19 11.0002H7.82998L12.71 6.12021C13.1 5.73021 13.1 5.09021 12.71 4.70021C12.32 4.31021 11.69 4.31021 11.3 4.70021L4.70998 11.2902C4.31998 11.6802 4.31998 12.3102 4.70998 12.7002L11.3 19.2902C11.69 19.6802 12.32 19.6802 12.71 19.2902C13.1 18.9002 13.1 18.2702 12.71 17.8802L7.82998 13.0002H19C19.55 13.0002 20 12.5502 20 12.0002C20 11.4502 19.55 11.0002 19 11.0002Z" fill="#8D191D"/>
                                         </svg>
                                     </div>
-                                    <h3>Удалить выбранные?</h3>
+                                    <h3>Очистить корзину?</h3>
                                 </div>
                             </div>
                             <div className="modal__content gap-8">
                                 <p>
-                                    Вы точно хотите удалить выбранные? Отменить данное действие будет невозможно.
+									Вы точно хотите очистить корзину? Отменить данное действие будет невозможно.
                                 </p>
                                 <button onClick={() =>basketUseCase.deliteCart()} className="btn btn-sm btn-red no-drag">
-                                    Удалить
+                                    Очистить корзину
                                 </button>
                                 <button onClick={() => setIsModalDeleteOpened(false)} className="btn btn-sm btn-gray no-drag">
                                     Отменить
