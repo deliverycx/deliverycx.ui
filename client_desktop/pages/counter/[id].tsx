5f114890374554265be15db5b58869460f810acf
xirurgx3d <xirurgx3d@gmail.com>
2024-04-24 18:22:32 +0300

    Update [id].tsx

--- client_desktop/pages/counter/[id].tsx
+++ client_desktop/pages/counter/[id].tsx
@@ -112,7 +112,7 @@ const CouterPage = () =>{
 				timercoutn = setInterval(async () => {
 					console.log('iiiiiiii');
 					await organizationCoutn(org.guid)
-				}, 15000)
+				},20000)
 				
 
 			}
