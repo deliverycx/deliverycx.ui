d986497921adccd2834fe2b7067bc0448bbc79bf
khinkalich <xirurgx3d@gmail.com>
2024-09-05 12:44:54 +0300

    Update AdressInfo.tsx

--- client/src/presentation/viewModel/viewHead/AdressInfo.tsx
+++ client/src/presentation/viewModel/viewHead/AdressInfo.tsx
@@ -5,15 +5,15 @@ import { useHistory } from "react-router-dom";
 import { adapterSelector } from "servises/redux/selectors/selectors";
 
 const AdressInfo: FC = () => {
-    const point = adapterSelector.useSelectors<IPoint>(selector => selector.point)
-    const history = useHistory();
-    return (
-        <div className="adress_info">
-            <div className="adress_info__city" onClick={()=>history.push(ROUTE_APP.MAIN)}>{point.city}</div>
-            <div className="adress_info__street" onClick={()=>history.push(ROUTE_APP.MAIN)}>{point.address}</div>
-            <a href={"tel:" + point.phone} className="adress_info__phone">{point.phone}</a>
-        </div> 
-    )
+	const point = adapterSelector.useSelectors<IPoint>(selector => selector.point)
+	const history = useHistory();
+	return (
+		<div className="adress_info">
+			<a href="хинкалыч.рф" className="adress_info__city" >{point.city}</a>
+			<a href="хинкалыч.рф" className="adress_info__street" >{point.address}</a>
+			<a href={"tel:" + point.phone} className="adress_info__phone">{point.phone}</a>
+		</div>
+	)
 }
 
 export default memo(AdressInfo);
