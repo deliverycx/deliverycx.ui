2265aea79c67892683f74874065507e63e6d9e36
khinkalich <xirurgx3d@gmail.com>
2024-11-20 12:21:33 +0300

    Update init-dev-letsencrypt.sh

--- init-dev-letsencrypt.sh
+++ init-dev-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(xn--80aimpg.xn--e1aybc.xn--80apgfh0ct5a.xn--p1ai)
+domains=(xn--80achc3cm7c.xn--e1aybc.xn--80apgfh0ct5a.xn--p1ai)
 rsa_key_size=4096
 data_path="./certbot"
 email="" # Adding a valid address is strongly recommended
