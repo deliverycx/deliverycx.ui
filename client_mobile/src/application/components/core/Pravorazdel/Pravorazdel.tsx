d3fdba4296dd0acc4783aa37b9a5103f190c39fd
khinkalich <xirurgx3d@gmail.com>
2025-02-04 12:47:33 +0300

    документы

--- client_mobile/src/application/components/core/Pravorazdel/Pravorazdel.tsx
+++ client_mobile/src/application/components/core/Pravorazdel/Pravorazdel.tsx
@@ -5,73 +5,80 @@ import { Mobile } from 'application/ResponseMedia';
 import HeaderBack from '../../common/HeaderBack/HeaderBack';
 
 const Pravorazdel = () => {
-  return (
-    <div>
-      <Mobile>
-        <HeaderBack>
-          <div className="header__logo">
-            <img src={require('assets/images/logo.jpg')} alt="Логотип" />
-          </div>
-        </HeaderBack>
-      </Mobile>
+	return (
+		<div>
+			<Mobile>
+				<HeaderBack>
+					<div className="header__logo">
+						<img src={require('assets/images/logo.jpg')} alt="Логотип" />
+					</div>
+				</HeaderBack>
+			</Mobile>
 
-      <div className="text_atom_mobile">Правовая информация</div>
+			<div className="text_atom_mobile">Правовая информация</div>
 
-      <div className="hrefs_mobile">
-        <div className={'href_mobile'}>
-          <a href="/docs/Пользовательское%20соглашение.pdf" target="_blank">
-            Пользовательское соглашение
-          </a>{' '}
-        </div>
-        <br />
-        <div className={'href_mobile'}>
-          <a href="/docs/cx.pdf" target="_blank">
-            Условия обработки персональные данных
-          </a>{' '}
-        </div>
-        <br />
-        <div className={'href_mobile'}>
-          <a href="/docs/Порядок%20возврата.pdf" target="_blank">
-            Порядок возврата
-          </a>
-        </div>
-        <br />
+			<div className="hrefs_mobile">
+				<div className={'href_mobile'}>
+					<a href="/docs/Пользовательское%20соглашение.pdf" target="_blank">
+						Пользовательское соглашение
+					</a>{' '}
+				</div>
+				<br />
+				<div className={'href_mobile'}>
+					<a href="/docs/cx.pdf" target="_blank">
+						Условия обработки персональные данных
+					</a>{' '}
+				</div>
+				<br />
+				<div className={'href_mobile'}>
+					<a href="/docs/Порядок%20возврата.pdf" target="_blank">
+						Порядок возврата
+					</a>
+				</div>
+				<br />
 
-        <div className={'href_mobile'}>
-          <a href="/docs/Акции%20сети%20кафе.pdf" target="_blank">
-            Акция сети кафе
-          </a>{' '}
-        </div>
-        <br />
+				<div className={'href_mobile'}>
+					<a href="/docs/Акции%20сети%20кафе.pdf" target="_blank">
+						Акция сети кафе
+					</a>{' '}
+				</div>
+				<br />
 
-        <div className={'href_mobile'}>
-          <a
-            href="/docs/Порядок_приобретения_и_использования_Подарочных_сертификатов_1_1.docx"
-            target="_blank"
-          >
-            Порядок приобретения и использования Подарочных сертификатов
-          </a>{' '}
-        </div>
-        <br />
+				<div className={'href_mobile'}>
+					<a
+						href="/docs/Порядок_приобретения_и_использования_Подарочных_сертификатов_1_1.docx"
+						target="_blank"
+					>
+						Порядок приобретения и использования Подарочных сертификатов
+					</a>{' '}
+				</div>
+				<br />
 
-        <div className={'href_mobile'}>
-          <a href="https://disk.yandex.ru/d/4kazVwmdSm85HQ" target="_blank">
-            Правила посещения хинкальной
-          </a>{' '}
-        </div>
-        <br />
+				<div className={'href_mobile'}>
+					<a href="https://disk.yandex.ru/d/4kazVwmdSm85HQ" target="_blank">
+						Правила посещения хинкальной
+					</a>{' '}
+				</div>
+				<br />
 
-        <div className={'href_mobile'}>
-          <a
-            href="/docs/Региональная%20Акция%20Приведи%20друга.pdf"
-            target="_blank"
-          >
-            Региональная акция- Приведи друга
-          </a>{' '}
-        </div>
-      </div>
-    </div>
-  );
+				<div className={'href_mobile'}>
+					<a href="/docs/Оферта.pdf" target="_blank">
+						Оферта
+					</a>{' '}
+				</div>
+				<br />
+
+				<div className={'href_mobile'}>
+					<a
+						href="/docs/Региональная%20Акция%20Приведи%20друга.pdf"
+						target="_blank"
+					>
+						Региональная акция- Приведи друга
+					</a>{' '}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Pravorazdel;
