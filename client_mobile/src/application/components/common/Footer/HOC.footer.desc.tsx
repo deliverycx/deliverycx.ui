9ed7835ac923015547d4fa5bf2eab9a5b97bae81
khinkalich <xirurgx3d@gmail.com>
2025-09-02 13:32:17 +0300

    q

--- client_mobile/src/application/components/common/Footer/HOC.footer.desc.tsx
+++ client_mobile/src/application/components/common/Footer/HOC.footer.desc.tsx
@@ -67,6 +67,12 @@ const HOCFooterDesc = () => {
 							>
 								Cтать тайным гостем
 							</a>
+							<a
+								href="/docs/mm.pdf"
+								className="foot_menu-item"
+							>
+								Исследования
+							</a>
 						</div>
 						<div className="foot_menu">
 							<div className="foot_menu-title">Контакты</div>
