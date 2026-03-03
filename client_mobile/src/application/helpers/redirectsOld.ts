e42642cc7abec5f82aac8ee2db33f85d8aba1af2
khinkalich <xirurgx3d@gmail.com>
2026-03-03 17:28:36 +0300

    Update redirectsOld.ts

--- client_mobile/src/application/helpers/redirectsOld.ts
+++ client_mobile/src/application/helpers/redirectsOld.ts
@@ -1,7 +1,7 @@
 export const Redirects = async (adress: any) => {
-  //const {data} = await RequestAdmin.getBu({idorganization:id})
-  //console.log(adress.redirectON);
-  if (adress.redirectON) {
-    window.location.href = `https://м.доставка.хинкалыч.рф/shop?organuzation=${adress.redirectUrl}`;
-  }
+	//const {data} = await RequestAdmin.getBu({idorganization:id})
+	//console.log(adress.redirectON);
+	if (adress.redirectON) {
+		window.location.href = `https://хинкалыч.рф`;
+	}
 };
